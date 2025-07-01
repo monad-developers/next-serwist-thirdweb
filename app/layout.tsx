d57181e4b2bd85e33e4f72985f772d14014a9bfb
portdeveloper <108868128+portdeveloper@users.noreply.github.com>
2025-07-01 10:16:42 +0300

    Basic auth using thirdweb

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata, Viewport } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { ThirdwebProvider } from "thirdweb/react";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -67,7 +68,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ThirdwebProvider>
+          {children}
+        </ThirdwebProvider>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 
 import SendNotification from "./components/SendNotification";
 import { InstallPWA } from "./components/InstallPWA";
+import { ConnectButton } from "thirdweb/react";
+import { client } from "./client";
 
 export const metadata: Metadata = {
   title: "Home",
@@ -14,13 +16,22 @@ export default function Page() {
         <div className="text-center mb-12">
           <div className="flex items-center justify-center mb-4">
             <h1 className="text-4xl md:text-5xl font-bold text-slate-800 dark:text-slate-100">
-              Next.js + Serwist
+              Next.js + Serwist + Thirdweb
             </h1>
           </div>
         </div>
         
         <div className="space-y-8">
           <SendNotification />
+          <div className="flex justify-center mb-20">
+          <ConnectButton
+            client={client}
+            appMetadata={{
+              name: "Example App",
+              url: "https://example.com",
+            }}
+          />
+        </div>
         </div>
       </div>
       <InstallPWA />
